d73631c7110984c0db1f21b37dff7949ec902018
Sebastian Sosa <sosasebastian667@gmail.com>
2024-03-20 23:16:35 -0600

    Primer commit

--- src/Container/Container.jsx
+++ src/Container/Container.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    /* position: absolute;
+    position: absolute;
     top: 0;
     left: 0;
     padding: 0;
@@ -11,7 +11,7 @@ const Container = styled.div`
     width: 100vw;
     display: flex;
     justify-content: center;
-    align-items: center; */
+    align-items: center;
 `;
 
 export default Container
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -9,7 +9,6 @@ function Dashboard() {
             <Navbar />
             <Container>
                 <Card>
-
                 </Card>
             </Container>
         </>
